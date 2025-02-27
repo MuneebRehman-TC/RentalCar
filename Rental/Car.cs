10a7e7e76a58f4b311f35d53b26b30c4832792c0
munee <munee@DESKTOP-ER2R0SE>
2025-02-27 12:54:14 +0500

    Add project files.

---
+++ Rental/Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rental
+{
+    public class Car
+    {
+        private static List<Car> cars = new List<Car>();
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public Car() { }
+
+        public Car(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+
+        public void Create()
+        {
+            var newCar = new Car(Brand, Model);
+            cars.Add(newCar);
+            Console.WriteLine("Car has Added");
+        }
+
+        public void Delete()
+        {
+            var car = cars.Find(c => c.Brand == Brand && c.Model == Model);
+            if (car != null)
+            {
+                cars.Remove(car);
+                Console.WriteLine($"Car {Brand} {Model} deleted.");
+            }
+            else
+            {
+                Console.WriteLine("Car not found.");
+            }
+        }
+
+        public void Update()
+        {
+            var car = cars.Find(c => c.Brand == Brand && c.Model == Model);
+            if (car != null)
+            {
+                Console.WriteLine("Enter new Brand:");
+                car.Brand = Console.ReadLine();
+                Console.WriteLine("Enter new Model:");
+                car.Model = Console.ReadLine();
+                Console.WriteLine("Car updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Car not found.");
+            }
+        }
+
+        public void Get()
+        {
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("No cars available.");
+            }
+            else
+            {
+                Console.WriteLine("Available cars:");
+                foreach (var ar in cars)
+                {
+                    Console.WriteLine($"Brand: {ar.Brand}, Model: {ar.Model}");
+                }
+            }
+        }
+    }
+}
---
+++ Rental/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rental
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            string continueOption;
+            do
+            {
+                Console.WriteLine("Select Option");
+                Console.WriteLine("1. Create");
+                Console.WriteLine("2. Update");
+                Console.WriteLine("3. Delete");
+                Console.WriteLine("4. Get");
+                Console.WriteLine();
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter Brand");
+                        car.Brand = Console.ReadLine();
+                        Console.WriteLine("Enter Model");
+                        car.Model = Console.ReadLine();
+                        car.Create();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Brand");
+                        car.Brand = Console.ReadLine();
+                        Console.WriteLine("Enter Model");
+                        car.Model = Console.ReadLine();
+                        car.Update();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter Brand");
+                        car.Brand = Console.ReadLine();
+                        Console.WriteLine("Enter Model");
+                        car.Model = Console.ReadLine();
+                        car.Delete();
+                        break;
+                    case 4:
+                        car.Get();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+
+           
+                do
+                {
+                    Console.WriteLine("Do you want to continue? (Y/N)");
+                    continueOption = Console.ReadLine().ToUpper();
+
+                    if (continueOption != "Y" && continueOption != "N")
+                    {
+                        Console.WriteLine("Invalid input. Please enter 'Y' for yes or 'N' for no.");
+                    }
+
+                } while (continueOption != "Y" && continueOption != "N");
+
+            } while (continueOption == "Y");
+        }
+    }
+}
